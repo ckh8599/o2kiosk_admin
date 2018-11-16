122e935a62163b71ed3dbfc52df8dc4b7701641a
ckh8599 <ckh8599@hanmail.net>
2018-11-16 09:41:00 +0900

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { CountdownModule } from 'ngx-countdown';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
