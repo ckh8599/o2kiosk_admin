13eb31bff9d55c069a3712e95a27e62be3b09a72
jh11425 <jh11425@naver.com>
2018-11-19 10:34:06 +0900

    sldkfjsldf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,30 @@ import { CountdownModule } from 'ngx-countdown';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    //웹소켓 오픈
+    this.openWebsocket();
+  }
 
+  //WebSocket 오픈
+  webSocket : WebSocket;
+  openWebsocket(){
+    this.webSocket  = new WebSocket("ws://110.45.199.181:8002/WS?token=MY_STORE&id=ADMIN");
+    
+    this.webSocket.onopen = function(message){
+      console.log("===== OPEN =====");
+    }
+
+    this.webSocket.onclose = function(message){
+      console.log("===== CLOSE =====");
+    }
+
+    this.webSocket.onerror = function(message){
+      console.log("!!!!! Error !!!!!");
+    }
+
+    this.webSocket.onmessage = function(message){
+      console.log("message : " + message.data);
+    }
   }
 
 }
